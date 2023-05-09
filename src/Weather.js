465061b22837e7bc738d8662fa6575850583e65d
Elham Estedlali <e.estedlali@gmail.com>
2023-05-09 22:17:56 +0430

    updated fetching data from api

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import SearchIcon from "./SearchIcon";
 import "./Weather.css";
+//var fetchState = "none";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
-    //alert("handle response start");
+    //fetchState = "ready";
+    console.log("response success: "); //delete it
+    console.log(response.data); //delete it
+
     setWeatherData({
       ready: true,
       cityName: response.data.name,
@@ -22,51 +26,40 @@ export default function Weather(props) {
       description: response.data.weather[0].description,
       icon: response.data.weather[0].icon,
     });
-    console.log(response.data);
-    //alert(`handle response end! ${weatherData.ready} and ${weatherData.temperature}`);
   }
 
   function callWeatherApi() {
+    console.log("call weather api...");
+    //fetchState = "running";
     const apiKey = "93d43dfe3b4a950e5b187e5dc313705e";
     const units = "metric";
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
-    //console.log(apiUrl);
-
     Axios.get(apiUrl)
       .then(handleResponse)
       .catch((error) => {
-        console.log(error);
-        alert("Sorry, an error occurred while fetching weather data.");
+        console.error("Sorry, an error occurred while fetching weather data.");
+        console.error(error);
+        //fetchState = "none";
       });
   }
 
-  useEffect(() => {
-    //alert("im in use effect");
-    callWeatherApi();
-  }, []);
-
   function handleCityChange(event) {
     setCity(event.target.value); //bejash bayad dar handleSubmit bashad
-    //alert("city is set");
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    //alert(`I'm in handleSubmit: city is: ${city}`);
     callWeatherApi();
   }
 
-  //alert(`I'm before if, ${weatherData.ready} and ${weatherData.temperature}`);
-
+  //if (fetchState === "ready") {
   if (weatherData.ready) {
-    //alert(`I'm in if! ${weatherData.ready} and ${weatherData.temperature}`);
-
+    //console.log("ready now!!!" + fetchState);
+    console.log("page rendering...");
     return (
       <div className="Weather">
         <div className="container">
           <WeatherInfo data={weatherData} />
-
           <form className="search-container" onSubmit={handleSubmit}>
             <input
               type="search"
@@ -82,8 +75,9 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    //alert("call weather api with default city...");
-    //callWeatherApi();
+    //if (fetchState === "none") {
+    console.log("else statement...");
+    callWeatherApi();
     return <div>Loading...</div>;
   }
 }

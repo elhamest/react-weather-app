fd94b186d2a8bd822451cd0238407ab161f9c59b
Elham Estedlali <e.estedlali@gmail.com>
2023-05-09 22:30:18 +0430

    Added footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ export default function App() {
     <div className="App">
       <h1> Weather Forecast </h1>
       <Weather defaultCity="Tehran" />
+      <footer>
+        <p>
+          <a
+            href="https://github.com/elhamest/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/elham-estedlali/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Elham Estedlali
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
